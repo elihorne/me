329a34777e0deecf4ef6b1c3ea420360d592e598
Eli Horne <elihorne@gmail.com>
2012-04-18 21:14:07 -0400

    swapping google analytics for chartbeat

--- public/js/function.js
+++ public/js/function.js
@@ -1,17 +1,5 @@
 $ = jQuery;
 
-function analytics() {
-    var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-452488-2']);
-      _gaq.push(['_trackPageview']);
-        
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-};
-
 function randomFart() {
     var randomFart = Math.floor(Math.random() * 6 + 1);
     document.getElementById('frap' + randomFart).play();
@@ -29,10 +17,6 @@ $('.audio-drawer').html(audioContent);
 }
 
 audioSetup();
-
-	
-analytics();
-
 
 init = function(){
 
